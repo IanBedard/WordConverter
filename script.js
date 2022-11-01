5d23f9b11376ce4c975f5de7371f27c070fd8840
IanBedard <ian.bedard@hotmail.com>
2022-10-31 22:24:49 -0400

    added files

---
+++ script.js
@@ -0,0 +1,53 @@
+(function() {
+    document.getElementById("document")
+        .addEventListener("change", handleFileSelect, false);
+        
+    function handleFileSelect(event) {
+        readFileInputEventAsArrayBuffer(event, function(arrayBuffer) {
+            mammoth.convertToHtml({arrayBuffer: arrayBuffer})
+      
+                .then(displayResult)
+                .done();
+        });
+    }
+    
+    function displayResult(result) {
+
+
+rgxUntilH1 = /.+?(?=<h1>)/
+        let output = result.value;
+
+output = output.replace(rgxUntilH1,"");
+  
+        document.getElementById("output").innerHTML = output;
+        
+        var messageHtml = result.messages.map(function(message) {
+            return '<li class="' + message.type + '">' + escapeHtml(message.message) + "</li>";
+        }).join("");
+        
+        document.getElementById("messages").innerHTML = "<ul>" + messageHtml + "</ul>";
+    }
+    
+    function readFileInputEventAsArrayBuffer(event, callback) {
+        var file = event.target.files[0];
+
+        var reader = new FileReader();
+        
+        reader.onload = function(loadEvent) {
+            var arrayBuffer = loadEvent.target.result;
+            callback(arrayBuffer);
+        };
+        
+        reader.readAsArrayBuffer(file);
+    }
+
+    function escapeHtml(value) {
+        return value
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+    }
+  
+    
+})();
